c2fa8117a79e7e6e82d3c32149adc236ca633da2
Gerald McAuley <geraldmcauley@gmail.com>
2018-02-01 15:53:52 +0000

    Working Calculator
    Working Calculator code. Still a few bugs and I want to fix layout but it does the basics :)

--- CalculatorProject/script.js
+++ CalculatorProject/script.js
@@ -1,8 +1,54 @@
+var finalCalculation =[];
+var totalString="";
+
+var display =document.getElementById("main-display").innerHTML;
+//shows the button pressed on the calc display
 function myFunction(input) {
     document.getElementById("main-display").innerHTML +=input;
+    finalCalculation.push(input);      	     
+}
+//clears the screen
+function myClearFunction(input) {
+    document.getElementById("main-display").innerHTML ="";
+    finalCalculation=[];  
+    totalString ="0";  	     
+}
+//adds numbers
+function myAddFunction(input) {
+    document.getElementById("main-display").innerHTML += "+"; 
+    finalCalculation.push(input);       
+}
+//subtract numbers
+function mySubtractFunction(input) {
+    document.getElementById("main-display").innerHTML += "-"; 
+    finalCalculation.push(input);       
+}
+//multiply numbers
+function myMultplyFunction(input) {
+    document.getElementById("main-display").innerHTML += "*"; 
+    finalCalculation.push(input);      
+}
+//divide numbers
+function myDivideFunction(input) {
+    document.getElementById("main-display").innerHTML += "/"; 
+    finalCalculation.push(input);       
+}
+//calculate all in the array
+function myEqualsFunction(input) {       
+ 	console.log(finalCalculation);
+ 	finalCalculation.forEach(function(item){ 		
+ 			if(item!=""){
+ 				totalString+=item;
+ 			} 			
+ 	}); 
+ 	console.log(eval(totalString));
+ 	document.getElementById("main-display").innerHTML =String(eval(totalString));
+ 	finalCalculation=[];
+ 	totalString=String(eval(totalString)); 	
 }
 
-//treat operand differently
-function operand(){
-	
-}
+
+
+
+
+
