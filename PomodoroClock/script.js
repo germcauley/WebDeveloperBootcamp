fb4a2ae80ff192abcdf9e4bbbcd7a5705d8b35f3
Gerald McAuley <geraldmcauley@gmail.com>
2018-02-04 22:49:14 +0000

    working clock

--- PomodoroClock/script.js
+++ PomodoroClock/script.js
@@ -1,3 +1,73 @@
-myFunction(){
-	document.getElementById("#plus").innerHTML ="132456789";
+var secCount = 0;
+var minCount =0;
+var distance=25
+var intervalID = null;
+
+
+function myAddFunction(){
+  if(secCount<59){
+    secCount+=1;
+    document.getElementById("secDiv").innerHTML=secCount;
+  }
+  else if(minCount<25){
+    minCount+=1;
+    secCount=0;
+    document.getElementById("minDiv").innerHTML=minCount;
+    document.getElementById("secDiv").innerHTML=secCount;
+  }
+	
+	
 }
+
+function mySubFunction(){
+	if(secCount>0){
+	secCount-=1;
+	document.getElementById("secDiv").innerHTML=secCount;
+	}
+  else if(minCount>0){
+    minCount-=1;
+    secCount=59;
+    document.getElementById("minDiv").innerHTML=minCount;
+    document.getElementById("secDiv").innerHTML=secCount;
+  }
+}
+
+
+function myTimer(){
+  intervalID = setInterval("showalert()", 1000);//pass function and timespan to setInterval
+
+}
+
+function showalert(){
+  if(minCount!=0&&secCount==0){
+     minCount-=1;
+    document.getElementById("minDiv").innerHTML=minCount;
+    secCount=59;
+    document.getElementById("secDiv").innerHTML=secCount;
+  }
+  else if(minCount!=0&&secCount!=0){
+    secCount-=1;
+    document.getElementById("secDiv").innerHTML=secCount;
+  }
+  else if(minCount==0&&secCount!=0){
+    secCount-=1;
+    document.getElementById("secDiv").innerHTML=secCount;
+  }
+  else {
+    alert("Countdown has ended!");
+    clearInterval(intervalID);
+    
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
