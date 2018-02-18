a577894e4c473ed48c838ce8517f8575a76a9154
Gerald McAuley <geraldmcauley@gmail.com>
2018-02-18 23:34:10 +0000

    ToDoList Project

---
+++ ToDoList/assets/js/script.js
@@ -0,0 +1,41 @@
+// Check off specific todos by clicking 
+//add to ul parent so works for dynamically added li elements
+
+$("ul").on("click","li",function(){
+	$(this).toggleClass('completed');
+});
+
+
+
+//Click on X to delete to do, applied to ul so anytime span inside ul 
+//is clicked it works for dynamically added elements
+$("ul").on("click","span",function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();//stops bubbling of events
+});
+
+//ul click to demonstrate click event bubble
+// $("ul").click(function(){
+// 	alert("ul clicked");
+// });
+
+//add to the list
+$("input[type='text']").keypress(function(event) {
+	if(event.which === 13){//13 is the code for the enter key
+		var todoText = ($(this).val());//grab input value when hit 
+		//clear the input
+		$(this).val("");
+		//create a new li with a span inside and trash icon inside span and add to ul
+		$("ul").append("<li><span><i class='fas fa-trash-alt'></i></span> "+todoText+"</li>");
+	}
+});
+
+
+$(".fas fa-pencil-alt").click(function(){
+	$("input").fadeToggle();
+});
+
+
+
