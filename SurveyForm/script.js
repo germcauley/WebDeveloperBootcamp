59e173888cd042997ac46b18259eac287882de63
Gerald McAuley <geraldmcauley@gmail.com>
2018-06-14 14:53:13 +0100

    Basic form, no backend

---
+++ SurveyForm/script.js
@@ -0,0 +1,16 @@
+
+//Number field validation
+// function numValidate() {
+//     var x, text;
+
+//     // Get the value of the input field with id="numb"
+//     x = document.getElementById("num").value;
+
+//     // If x is Not a Number or less than one or greater than 10
+//     if (x.length >10 || x.length <10) {
+//         alert("Input not valid, number must be 10 digits exactly");
+//     } else {
+//         alert("Input OK");
+//     }
+    
+// }
